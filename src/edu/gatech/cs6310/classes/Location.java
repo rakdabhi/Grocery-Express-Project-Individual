c22a6b83679d494683b088d5d95e01079c47d93e
rakdabhi <rakdabhi@gmail.com>
2023-11-05 15:05:02 -0500

    Update Location class and override compareTo() and equals() methods to make Locations comparable

--- src/edu/gatech/cs6310/classes/Location.java
+++ src/edu/gatech/cs6310/classes/Location.java
@@ -1,4 +1,35 @@
 package edu.gatech.cs6310.classes;
 
-public class Location {
+public class Location implements Comparable<Location> {
+    private int x, y;
+
+    public Location(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Location thatLocation)) {
+            return false;
+        }
+        return this.x == thatLocation.getX() && this.y == thatLocation.getY();
+    }
+
+    @Override
+    public int compareTo(Location thatLocation) {
+        if (this.x != thatLocation.getX()) {
+            return Integer.compare(this.x, thatLocation.getX());
+        } else {
+            return Integer.compare(this.y, thatLocation.getY());
+        }
+    }
 }
