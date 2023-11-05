599f1956bec30201cc97ec04fc9311be9fec733a
rakdabhi <rakdabhi@gmail.com>
2023-11-05 14:30:52 -0500

    Change variable names and update documentation

--- src/edu/gatech/cs6310/classes/Clock.java
+++ src/edu/gatech/cs6310/classes/Clock.java
@@ -23,11 +23,11 @@ public class Clock {
      * f(x) = -720 * cos((2π/1440)x) + 720
      * Thus, the amount of sunlight emitted within a given timeframe is the integral
      * over f(x) from startTime to endTime.
-     * NOTE: Integral of above function is: -(518400/π) * sin((π/720)x) + 720x + C
+     * NOTE: Integral of above function is: ∫f(x)dx = -(518400/π) * sin((π/720)x) + 720x + C
      * NOTE: The above equation has a period of 1440 to replicate 1440 minutes in a day
      *       so that startTime and endTime can be provided with minute units.
-     * @param startTime - start time of timeframe
-     * @param endTime - end time of timeframe
+     * @param startTime - start time of timeframe in minutes
+     * @param endTime - end time of timeframe in minutes
      * @return amount of daylight emitted from start time to end time
      */
     public static int getLightOverTime(int startTime, int endTime) {
@@ -41,12 +41,12 @@ public class Clock {
     /**
      * Helper method to help calculate the amount of sunlight at a given time.
      * This function is the integral of the daylight simulation function mentioned above.
-     * Integral: -(518400/π) * sin((π/720)x) + 720x + C
+     * Integral: ∫f(x)dx = -(518400/π) * sin((π/720)x) + 720x + C
      * NOTE: Constant 'C' gets cancelled out during integration.
-     * @param time - point of time to calculate the amount of daylight
+     * @param x - input to ∫f(x)dx, point of time to calculate the amount of daylight
      * @return the amount of daylight at the given time
      */
-    private static double daylightFunc(int time) {
-        return -(518400/Math.PI) * Math.sin((Math.PI/720) * time) + (720 * time);
+    private static double daylightFunc(int x) {
+        return -(518400/Math.PI) * Math.sin((Math.PI/720) * x) + (720 * x);
     }
 }
