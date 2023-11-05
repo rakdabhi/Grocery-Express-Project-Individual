119e0ae4fe6bdca5f94786ab3f34ff6ebe0acd88
rakdabhi <rakdabhi@gmail.com>
2023-11-05 11:36:42 -0500

    Create new Clock class and define the day/night cycles function

---
+++ src/edu/gatech/cs6310/classes/Clock.java
@@ -0,0 +1,52 @@
+package edu.gatech.cs6310.classes;
+
+public class Clock {
+    private int time;
+
+    public Clock() {
+        this.time = 0;
+    }
+
+    public int getTime() {
+        return this.time;
+    }
+
+    public void incrementTime(int delta) {
+        this.time += delta;
+    }
+
+    /**
+     * Returns the amount of sunlight that was emitted between a start time and end time.
+     * Daylight is modelled as a sine/cosine function to simulate real day/night cycles,
+     * with varying levels of sunlight throughout different times of the day.
+     * The exact function to simulate the amount of sunlight emitted throughout the day is:
+     * f(x) = -720 * cos((2π/1440)x) + 720
+     * Thus, the amount of sunlight emitted within a given timeframe is the integral
+     * over f(x) from startTime to endTime.
+     * NOTE: Integral of above function is: -(518400/π) * sin((π/720)x) + 720x + C
+     * NOTE: The above equation has a period of 1440 to replicate 1440 minutes in a day
+     *       so that startTime and endTime can be provided with minute units.
+     * @param startTime - start time of timeframe
+     * @param endTime - end time of timeframe
+     * @return amount of daylight emitted from start time to end time
+     */
+    public static int getLightOverTime(int startTime, int endTime) {
+        if (startTime >= endTime) {
+            System.out.printf("ERROR: Start time %d is greater than or equal to end time %d!", startTime, endTime);
+            return 0;
+        }
+        return (int) Math.round(daylightFunc(endTime) - daylightFunc(startTime));
+    }
+
+    /**
+     * Helper method to help calculate the amount of sunlight at a given time.
+     * This function is the integral of the daylight simulation function mentioned above.
+     * Integral: -(518400/π) * sin((π/720)x) + 720x + C
+     * NOTE: Constant 'C' gets cancelled out during integration.
+     * @param time - point of time to calculate the amount of daylight
+     * @return the amount of daylight at the given time
+     */
+    private static double daylightFunc(int time) {
+        return -(518400/Math.PI) * Math.sin((Math.PI/720) * time) + (720 * time);
+    }
+}
