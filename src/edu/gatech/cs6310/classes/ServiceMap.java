63fd85bb39dad7a18097c49c8d8c298f4b1e83a6
rakdabhi <rakdabhi@gmail.com>
2023-11-05 23:12:10 -0500

    Implement ServiceMap logic (added Constructor and several add functions and exists function)

--- src/edu/gatech/cs6310/classes/ServiceMap.java
+++ src/edu/gatech/cs6310/classes/ServiceMap.java
@@ -1,4 +1,70 @@
 package edu.gatech.cs6310.classes;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class ServiceMap {
+    private Map<Location, Customer> customerMap;
+    private Map<Location, Store> storeMap;
+    private Map<Location, Drone> droneMap;
+
+    public ServiceMap() {
+        this.customerMap = new TreeMap<Location, Customer>();
+        this.storeMap = new TreeMap<Location, Store>();
+        this.droneMap = new TreeMap<Location, Drone>();
+    }
+
+    /**
+     * Function to add a location-customer pairing to customerMap
+     * @param location - location of customer to add to customerMap
+     * @param customer - customer with given location
+     * @return true if customer is added to map, else false
+     */
+    public boolean addLocation(Location location, Customer customer) {
+        if (this.locationExists(location)) {
+            System.out.println("ERROR:location_already_taken");
+            return false;
+        }
+        this.customerMap.put(location, customer);
+        return true;
+    }
+
+    /**
+     * Function to add a location-store pairing to storeMap
+     * @param location - location of store to add to storeMap
+     * @param store - store with given location
+     * @return true if store is added to map, else false
+     */
+    public boolean addLocation(Location location, Store store) {
+        if (this.locationExists(location)) {
+            System.out.println("ERROR:location_already_taken");
+            return false;
+        }
+        this.storeMap.put(location, store);
+        return true;
+    }
+
+    /**
+     * Function to add a location-drone pairing to droneMap
+     * @param location - location of drone to add to droneMap
+     * @param drone - drone with given location
+     * @return true if drone is added to map, else false
+     */
+    public boolean addLocation(Location location, Drone drone) {
+        if (this.locationExists(location)) {
+            System.out.println("ERROR:location_already_taken");
+            return false;
+        }
+        this.droneMap.put(location, drone);
+        return true;
+    }
+
+    /**
+     * Determines whether a customer or store already exists at the given location
+     * @param location - location to check whether a customer or store is occupying that place
+     * @return true if a customer or store exists at the given location, else false
+     */
+    public boolean locationExists(Location location) {
+        return this.customerMap.containsKey(location) || this.storeMap.containsKey(location);
+    }
 }
